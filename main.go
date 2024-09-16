91b488f121e29aaa09443b3d8a0fef76b8afd4ac
HelloMeFriend <80709119+HelloMeFriend@users.noreply.github.com>
2024-09-16 08:49:31 -0400

    improve !play search

--- main.go
+++ main.go
@@ -29,9 +29,21 @@ func main() {
 		switch m.Content {
 		case "hello":
 			s.ChannelMessageSend(m.ChannelID, "world!")
+			return
+		case "!stop":
+			if voiceConnection != nil {
+				voiceConnection.Disconnect()
+			}
+		}
 
-		case "!play":
-			// Find the voice channel the user is in
+		srch := ""
+		if len(m.Content) > 6 {
+			srch = m.Content[0:6]
+		}
+
+		if srch == "!play " {
+			searchString := m.Content[5:]
+
 			guild, err := s.State.Guild(m.GuildID)
 			if err != nil {
 				log.Println("Error finding guild:", err)
@@ -62,15 +74,23 @@ func main() {
 			voiceConnection = vc
 			s.ChannelMessageSend(m.ChannelID, "Joined the voice channel!")
 
-			link, err := SearchYoutube("3hunna", m)
+			link, err := SearchYoutube(searchString, m)
+			if err != nil {
+				s.ChannelMessageSend(m.ChannelID, "Failed to find request.")
+				return
+			}
+
 			s.ChannelMessageSend(m.ChannelID, link.VideoURL)
 
-			// Remember to disconnect from the voice channel when done
-
-		case "!quit":
-			if voiceConnection != nil {
-				voiceConnection.Disconnect()
+			err = playAudio(vc, link.VideoURL)
+			if err != nil {
+				log.Println("Error playing audio:", err)
+				s.ChannelMessageSend(m.ChannelID, "Error playing audio.")
 			}
+
+			return
+
+			// Remember to disconnect from the voice channel when done
 		}
 	})
 
